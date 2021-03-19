6ebd65472c801b33c12071bdf919fa1f791f1b4f
Patrick Pfeiffer <patrick@bitfly.at>
2021-03-20 00:41:28 +0100

    use api endpoint, add -max flag

--- graffitiwallpainter.go
+++ graffitiwallpainter.go
@@ -6,9 +6,9 @@ import (
 	"fmt"
 	"image/png"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"os"
-	"regexp"
 	"runtime"
 	"strings"
 	"time"
@@ -23,6 +23,7 @@ var imageFile string
 var graffitiFile string
 var explorerURL string
 var runOnce bool
+var max int
 
 // Build information. Populated at build-time
 var (
@@ -34,15 +35,19 @@ var (
 )
 
 func main() {
-	flag.StringVar(&explorerURL, "url", "https://beaconcha.in/graffitiwall", "url to graffitiwall page of explorer")
+	flag.StringVar(&explorerURL, "url", "https://beaconcha.in/api/v1/graffitiwall", "url to graffitiwall page of explorer")
 	flag.DurationVar(&interval, "interval", time.Duration(32*12)*time.Second, "interval in which the graffiti-file will be updated")
 	flag.StringVar(&imageFile, "image", "image.png", "path to image")
 	flag.IntVar(&xOffset, "x", 0, "offset x")
 	flag.IntVar(&yOffset, "y", 0, "offset y")
 	flag.StringVar(&graffitiFile, "graffiti", "graffiti.txt", "path to graffiti-file")
 	flag.BoolVar(&runOnce, "once", false, "if set, write all pixels to file and exit")
+	flag.IntVar(&max, "max", 0, "if set to >0, only N pixels will be writen to file")
 	flag.Parse()
 
+	if max < 0 {
+		logrus.Fatal("max must be >=0")
+	}
 	if interval < time.Second*12 {
 		interval = time.Second * 12
 		logrus.Warnf("setting interval to %v, lower value does not make sense\n", interval)
@@ -84,7 +89,7 @@ func run(explorerURL string, gwWant map[string]string) error {
 	if err != nil {
 		return err
 	}
-	res := []string{"random:\n"}
+	res := []string{}
 	for xy, color := range gwWant {
 		colorIs, exists := gwIs[xy]
 		if color == "ffffff" && !exists {
@@ -98,7 +103,15 @@ func run(explorerURL string, gwWant map[string]string) error {
 		logrus.Infof("all pixels match the image!")
 		return nil
 	}
-	err = ioutil.WriteFile(graffitiFile, []byte(strings.Join(res, "")), 0644)
+	if max > 0 {
+		currmax := max
+		if currmax > len(res) {
+			currmax = len(res)
+		}
+		res = res[:currmax]
+		rand.Shuffle(currmax, func(i, j int) { res[i], res[j] = res[j], res[i] })
+	}
+	err = ioutil.WriteFile(graffitiFile, []byte("random:\n"+strings.Join(res, "")), 0644)
 	if err != nil {
 		return err
 	}
@@ -135,6 +148,17 @@ func readImage(file string, offsetX, offsetY int) (map[string]string, error) {
 	return res, nil
 }
 
+type APIResponse struct {
+	Status string `json:"status"`
+	Data   []struct {
+		X         uint32 `json:"x"`
+		Y         uint32 `json:"y"`
+		Color     string `json:"color"`
+		Validator uint64 `json:"validator"`
+		Slot      uint64 `json:"slot"`
+	}
+}
+
 func getGraffitiwall(explorerURL string) (map[string]string, error) {
 	client := &http.Client{Timeout: time.Second * 10}
 	resp, err := client.Get(explorerURL)
@@ -142,28 +166,21 @@ func getGraffitiwall(explorerURL string) (map[string]string, error) {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	data, err := ioutil.ReadAll(resp.Body)
+	dataBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
-	}
-	dataRE := regexp.MustCompile(`var pixels = (.*)\n`)
-	m := dataRE.FindAllStringSubmatch(string(data), -1)
-	if len(m) < 1 || len(m[0]) < 2 {
-		return nil, fmt.Errorf("could not read wall from beaconchain")
 	}
 
-	type graffitiJSONT []struct {
-		X     uint32
-		Y     uint32
-		Color string
-	}
-	var graffitiJSON graffitiJSONT
-	err = json.Unmarshal([]byte(m[0][1]), &graffitiJSON)
+	var data APIResponse
+	err = json.Unmarshal(dataBytes, &data)
 	if err != nil {
 		return nil, err
 	}
+	if data.Status != "OK" {
+		return nil, fmt.Errorf("api status not ok: %s", data.Status)
+	}
 	res := make(map[string]string)
-	for _, g := range graffitiJSON {
+	for _, g := range data.Data {
 		res[fmt.Sprintf("%d:%d", g.X, g.Y)] = g.Color
 	}
 	return res, nil
